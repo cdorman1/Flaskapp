121b33c4233d63dd25df58be492f58e321331fb6
cdorman1 <cdorman09@gmail.com>
2014-03-09 01:06:29 -0600

    updated html files

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
@@ -9,6 +9,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
