b0e9c90f3cda60ca208bcd3d8d0033d5b3095454
cdorman1 <cdorman09@gmail.com>
2014-03-11 23:22:15 -0500

    commit profile site

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/home')
 def home():
     return render_template('home.html')
+
 
 @app.route('/about')
 def about():
